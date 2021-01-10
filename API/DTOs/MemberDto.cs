0bd66b664636cc277b2a67006cf7d56191ee27ab
mavzerbay <mavzerbay@gmail.com>
2021-01-10 16:40:40 +0300

    End of section 9

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -9,7 +9,9 @@ namespace API.DTOs
         public string UserName { get; set; }
         public string PhotoUrl { get; set; }
         public int Age { get; set; }
-        public string KnownAs { get; set; }
+        public string KnownAs { get; set; }        
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
