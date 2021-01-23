a4c384ea4b45cce5df94d7e076c43d3af879e3cf
mavzerbay <mavzerbay@gmail.com>
2021-01-23 18:17:28 +0300

    End of section 14

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -36,7 +36,7 @@ export class MembersService {
     this.userParams = params;
   }
 
-  resetUserParams(){
+  resetUserParams() {
     this.userParams = new UserParams(this.user);
     return this.userParams;
   }
@@ -86,6 +86,16 @@ export class MembersService {
 
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + "likes/" + username, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = this.getPaginationHeaders(pageNumber, pageSize);
+    params = params.append("predicate", predicate);
+    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl + "likes", params);
   }
 
   private getPaginatedResult<T>(url, params) {
