540fd1363a1c3ce1b5c2f08423e505b5ba4b0c19
mavzerbay <mavzerbay@gmail.com>
2021-01-25 01:49:58 +0300

    End of section 17

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,6 +28,7 @@ namespace API
             services.AddControllers();
             services.AddCors();
             services.AddIdentityServices(Configuration);
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +41,10 @@ namespace API
             app.UseRouting();
 
             app.UseCors(
-                x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
+                x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200")
             );
 
             app.UseAuthentication();
@@ -49,6 +54,8 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -9,19 +9,18 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
-  @ViewChild("messageForm") messageForm:NgForm;
+  @ViewChild("messageForm") messageForm: NgForm;
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
 
-  constructor(private messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
   sendMessage() {
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message => {
-      this.messages.push(message);
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm.reset();
     });
   }
